661770fc10b789d022d6c129b71926fe804c8551
ckhrysze <ckhrysze@gmail.com>
2011-03-15 22:22:29 -0500

    first commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cdpmud::Application.config.session_store :cookie_store, :key => '_cdpmud_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cdpmud::Application.config.session_store :active_record_store
