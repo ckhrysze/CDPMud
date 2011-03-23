e61304e0c2c27d1daa662545e6c8838a75d1d799
ckhrysze <ckhrysze@gmail.com>
2011-03-22 23:48:30 -0500

    switching to node

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Cdpmud::Application.config.secret_token = '9365246c4a091443b65ab101cd094e6630b6566e32cef83154d135117a6f1624bcf924cd758a039d13ad7f42579a01ebd04b3306b9face9a7d1bb6f890d2251e'
